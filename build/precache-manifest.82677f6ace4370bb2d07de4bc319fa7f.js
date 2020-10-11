c53ac1aed7df86638f1bb5686e5cd57ae2d66d8f
Nikas <nikas@Nikas-MBP.lan>
2020-10-11 18:26:48 +0300

    build

---
+++ build/precache-manifest.82677f6ace4370bb2d07de4bc319fa7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "210cdf3c6e4b28d683357f1319280685",
+    "url": "/weatherAppReact2020/index.html"
+  },
+  {
+    "revision": "cb35a48e69a96d13d2ba",
+    "url": "/weatherAppReact2020/static/css/main.ca5361b0.chunk.css"
+  },
+  {
+    "revision": "cb18f9a689f2b667219a",
+    "url": "/weatherAppReact2020/static/js/2.61bfd103.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherAppReact2020/static/js/2.61bfd103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb35a48e69a96d13d2ba",
+    "url": "/weatherAppReact2020/static/js/main.e23407cc.chunk.js"
+  },
+  {
+    "revision": "fa5e3b1d36cdabcf7d2a",
+    "url": "/weatherAppReact2020/static/js/runtime-main.2d8ab680.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weatherAppReact2020/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weatherAppReact2020/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
